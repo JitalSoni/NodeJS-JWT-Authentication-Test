1aa99b405da3217604fc954802f260030a9fd8e8
Jital Soni <jsoni@uncc.edu>
2020-10-23 13:19:40 -0400

    NodeJS-JWT-Authentication Test

---
+++ server.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow_headers', 'Content-type,Authorization');
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+const PORT = 3000;
+
+const sercretKey = 'My super secret key';
+const jwtMW = exjwt({
+    secret: sercretKey,
+    algorithms: ['HS256']
+});
+
+let users = [
+    {
+    id: 1,
+    username: 'jital',
+    password: '123'
+    },
+    {
+    id: 2,
+    username: 'Soni',
+    password: '456'
+    }
+];
+
+app.post('/api/login', (req, res) => {
+    const {username, password} = req.body;
+
+    for(let user of users){
+        if(username == user.username && password == user. password){
+            let token = jwt.sign({ id: user.id, username: user.username }, sercretKey, {expiresIn: '7d'});
+            res.json({
+                success: true,
+                err: null,
+                token
+            });
+            break;
+        }
+        else{
+            res.status(401).json({
+                success: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+    }
+});
+
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'settings'
+    });
+});
+
+app.get('/api/dashboard', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'Secret content that only logged in people can see.'
+    });
+});
+
+app.get('/api/prices', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'This is the price $3.99'
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(function (err, req, res, next) {
+    //console.log(err.name === 'UnAuthorizedError');
+    //console.log(err);
+    if(err.name === 'UnauthorizedError') {
+        res.status(401).json({
+            success: false,
+            err
+        });
+    }
+        else{
+            next(err);
+        }
+    
+});
+
+app.listen(PORT, () => {
+    console.log(`Serving in port ${PORT}`);
+});
